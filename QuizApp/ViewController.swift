9f8ac6d8405071bd18ae646b035a10486f7d3d21
All3yp <noyallepereira@gmail.com>
2022-11-29 21:01:51 -0300

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Alley Pereira on 29/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
