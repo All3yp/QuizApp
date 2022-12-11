a618aa2c65e6b35ae61848a772abaa7d900bb61d
All3yp <noyallepereira@gmail.com>
2022-12-11 18:05:05 -0300

    WIP: Router URL

---
+++ QuizApp/Network/Router.swift
@@ -0,0 +1,40 @@
+//
+//  Router.swift
+//  QuizApp
+//
+//  Created by Alley Pereira on 11/12/22.
+//
+
+import Foundation
+
+enum Router {
+
+	/// Create new cases for more requests
+	case computerScience
+	case random
+//	case videoGames
+//	case music
+
+	static private let baseURL: String = "https://run.mocky.io/v3"
+
+	var url: URL {
+		URL(string: Self.baseURL+self.path)!
+	}
+
+	// MARK: - Paths
+	private var path: String {
+		switch self {
+			case .computerScience:
+				return "/825a9b94-d2e9-47ce-817d-65844f3582d8"
+			case .random:
+				return "/825a9b94-d2e9-47ce-817d-65844f3582d8"
+		}
+	}
+
+	// MARK: - HTTP Methods
+	var getRequest: URLRequest {
+		var request = URLRequest(url: self.url)
+		request.httpMethod = "GET"
+		return request
+	}
+}
