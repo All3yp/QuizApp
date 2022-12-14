af7498b4b7cedb111479ab3b9d36e0f3eaad5d30
All3yp <noyallepereira@gmail.com>
2022-12-13 23:22:44 -0300

    WIP: Create Loadview to populate views from the data that comes from the API

--- QuizApp/Components/Quiz/QuizViewController.swift
+++ QuizApp/Components/Quiz/QuizViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, QuizDelegate {
 
 	var coordinator: QuizCoordinator?
+	let quizView: QuizView = QuizView()
 
-	private let quizView: QuizView = QuizView()
+	private var questionAnswered: Int = 1
+	private lazy var viewModel = QuizViewModel(delegate: self)
 
+
+	// MARK:  - Lifecycle
 	override func loadView() {
 		self.view = quizView
 	}
@@ -21,13 +25,39 @@ class QuizViewController: UIViewController {
 		super.viewDidLoad()
 		self.view.backgroundColor = UIColor.CustomColor.dark
 		goToFinalResult()
+		self.viewModel.loadQuizQuestionsForComputerScience()
 	}
 
 	private func goToFinalResult() {
 		self.quizView.didTapBntToFinishQuiz = { [weak self] in
 			guard let self else { return }
-			self.coordinator?.coordinate(to: FinishQuizToResultCoordinator(viewController: self))
+			self.coordinator?.coordinate(
+				to: FinishQuizToResultCoordinator(viewController: self)
+			)
 		}
 	}
-	
+
+	// MARK:  - Load Data
+	func loadQuiz(with quiz: Quiz) {
+		// Extract the relevant data from the Quiz object
+		let result = quiz.results[questionAnswered - 1]
+		let answersViewModel = AnsweresViewModel(answer: result.incorrectAnswers + [result.correctAnswer], correctAnswer: result.correctAnswer)
+		let answers = answersViewModel.answer.shuffled()
+
+		// Update the quiz view with the extracted data
+		updateQuizView(questionAnswered: questionAnswered, quiz: quiz, result: result, answers: answers)
+	}
+
+	func updateQuizView(questionAnswered: Int, quiz: Quiz, result: QuizResult, answers: [String]) {
+		var answers = answers
+		DispatchQueue.main.async { [self] in
+			self.quizView.numberOfQuestionsLabel.text = "Question \(String(describing: questionAnswered)) of \(quiz.results.count)"
+			self.quizView.questionLabel.text = result.question
+			self.quizView.alternativeToAnswerBoardView01.answerAlternativeLabel.text = answers.popLast()
+			self.quizView.alternativeToAnswerBoardView02.answerAlternativeLabel.text = answers.popLast()
+			self.quizView.alternativeToAnswerBoardView03.answerAlternativeLabel.text = answers.popLast()
+			self.quizView.alternativeToAnswerBoardView04.answerAlternativeLabel.text = answers.popLast()
+		}
+	}
+
 }
